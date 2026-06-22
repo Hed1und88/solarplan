cbb802d676520085a675eca6f025f4397b2dc400
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-22 23:20:32 +0200

    Expose electrical products in battery planning

--- src/components/project/BatteryTab.jsx
+++ src/components/project/BatteryTab.jsx
@@ -1,1 +1,12 @@
-export { default } from './BatteryPlannerV3';
+import './BatteryPlannerGlobals';
+import BatteryPlannerV3 from './BatteryPlannerV3';
+import ElectricalProductQuickAdd from './ElectricalProductQuickAdd';
+
+export default function BatteryTab(props) {
+  return (
+    <div className="space-y-4">
+      <ElectricalProductQuickAdd />
+      <BatteryPlannerV3 {...props} />
+    </div>
+  );
+}
