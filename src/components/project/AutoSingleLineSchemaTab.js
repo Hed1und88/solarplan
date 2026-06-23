73dee65bbe557ca02f8d01e14dc000d6255dcd0b
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-23 12:31:44 +0200

    Normalize inverter selection for automatic single-line schema

---
+++ src/components/project/AutoSingleLineSchemaTab.js
@@ -0,0 +1,11 @@
+import React, { useMemo } from 'react';
+import AutoSingleLineSchemaTab from './AutoSingleLineSchemaTab.jsx';
+import { normalizeStringProductContext } from '@/lib/stringProductContext';
+
+export default function AutoSingleLineSchemaEntry(props) {
+  const context = useMemo(
+    () => normalizeStringProductContext(props.project || {}, props.products || []),
+    [props.project?.string_layout_data, props.project?.solar_roof_planner_data, props.project?.panel_layout_data, props.products],
+  );
+  return <AutoSingleLineSchemaTab {...props} project={context.project} products={context.products} />;
+}
