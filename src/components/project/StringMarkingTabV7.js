6117d642a3bd668dea44d3f7bc6b62fd0f4e07cc
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-23 12:09:02 +0200

    Make Slingor entrypoint compatible with Vite

--- src/components/project/StringMarkingTabV7.js
+++ src/components/project/StringMarkingTabV7.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import StringMarkingTabV7 from './StringMarkingTabV7.jsx';
 
 export default function StringMarkingTabWorkspace(props) {
-  return (
-    <div className="relative left-1/2 w-[calc(100vw-2rem)] max-w-[1800px] -translate-x-1/2">
-      <StringMarkingTabV7 {...props} />
-    </div>
+  return React.createElement(
+    'div',
+    { className: 'relative left-1/2 w-[calc(100vw-2rem)] max-w-[1800px] -translate-x-1/2' },
+    React.createElement(StringMarkingTabV7, props),
   );
 }
