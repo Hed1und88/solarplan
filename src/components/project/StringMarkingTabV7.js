a8f222c86b01aed272b4be99372be0f245a5a4c4
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-23 12:20:03 +0200

    Load inverter-driven Slingor workspace

--- src/components/project/StringMarkingTabV7.js
+++ src/components/project/StringMarkingTabV7.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import StringMarkingTabV7 from './StringMarkingTabV7.jsx';
-
-export default function StringMarkingTabWorkspace(props) {
-  return React.createElement(
-    'div',
-    { className: 'relative left-1/2 w-[calc(100vw-2rem)] max-w-[1800px] -translate-x-1/2' },
-    React.createElement(StringMarkingTabV7, props),
-  );
-}
+export { default } from './StringMarkingEntry.jsx';
