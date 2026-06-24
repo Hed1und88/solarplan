2f8cf0bb2ebad7871544f561d1e5f6f35b6877ca
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-24 13:54:36 +0200

    Add Flow reference totals

--- src/lib/flow/flowValidation.js
+++ src/lib/flow/flowValidation.js
@@ -1,1 +1,3 @@
-export const FLOW_REFERENCE_CASES=[];
+import { east\u0057estFieldHeightMm } from './flowEastWestGeometry.js';
+import { selectDockPosition } from './flowParallelGeometry.js';
+export const FLOW_REFERENCE_CASES=[{id:'planner-896',expectedB\u0061llastKg:896},{id:'planner-846',expectedB\u0061llastKg:846},{id:'planner-432',expectedB\u0061llastKg:432}];
