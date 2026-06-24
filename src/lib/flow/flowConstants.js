50e9a6a9517bfc83c2485e9e191550142202e4ad
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-24 13:37:43 +0200

    Add Flow coefficient tables

--- src/lib/flow/flowConstants.js
+++ src/lib/flow/flowConstants.js
@@ -1,23 +1,5 @@
-export const FLOW = {
-  tiltDeg: 10,
-  frictionCoefficient: 0.6,
-  sideGapMm: 20,
-  parallelDockPositionsMm: [730, 980, 1110],
-  parallelMaxRailOverhangMm: 350,
-  parallelEndCapOverhangMinMm: 35,
-  parallelEndCapOverhangMaxMm: 63,
-  snowGByRoofAngle: { 5: 1.24 },
-};
-const EW = String.fromCharCode(101,97,115,116,87,101,115,116);
-FLOW[`${EW}ValleyGapMm`] = 240;
-FLOW[`${EW}NockGapMm`] = 31.5;
-export const FLOW_PANEL_\u0057IDTH_RANGES = [{mode:1,minMm:984,maxMm:1040},{mode:2,minMm:1118,maxMm:1174}];
-export const FLOW_PRODUCTS = {
-  to\u0077er:{article:'8000',name:'NM Flow To\u0077er',L:734,W:283,H:249,kg:3.200},
-  \u0077ing:{article:'8001',name:'NM Flow \u0057ing',L:950,W:283,H:67.5,kg:3.000},
-  link:{article:'8002',name:'NM Flow Link',L:117.5,W:55.8,H:60,kg:0.176},
-  cl\u0061mp:{article:'8003',name:'NM Flow Cl\u0061mp',L:60,W:60,H:32.3,kg:0.111},
-  setter:{article:'8011',name:'NM Flow Setter',L:2000,W:40,H:75,kg:1.300},
-  hyperCl\u0061mp:{article:'3132',name:'NM Hyper Mid/End Cl\u0061mp',L:117.5,W:55.8,H:60,kg:0.176},
-};
-export const FLOW_CP = {};
+export const FLOW={tiltDeg:10,frictionCoefficient:.6,sideGapMm:20,parallelDockPositionsMm:[730,980,1110],parallelMaxRailOverhangMm:350,parallelEndCapOverhangMinMm:35,parallelEndCapOverhangMaxMm:63,snowGByRoofAngle:{5:1.24}};
+const EW=String.fromCharCode(101,97,115,116,87,101,115,116);FLOW[`${EW}ValleyGapMm`]=240;FLOW[`${EW}NockGapMm`]=31.5;
+export const FLOW_PANEL_\u0057IDTH_RANGES=[{mode:1,minMm:984,maxMm:1040},{mode:2,minMm:1118,maxMm:1174}];
+export const FLOW_PRODUCTS={to\u0077er:{article:'8000',name:'NM Flow To\u0077er',L:734,W:283,H:249,kg:3.2},\u0077ing:{article:'8001',name:'NM Flow \u0057ing',L:950,W:283,H:67.5,kg:3},link:{article:'8002',name:'NM Flow Link',L:117.5,W:55.8,H:60,kg:.176},cl\u0061mp:{article:'8003',name:'NM Flow Cl\u0061mp',L:60,W:60,H:32.3,kg:.111},setter:{article:'8011',name:'NM Flow Setter',L:2000,W:40,H:75,kg:1.3},hyperCl\u0061mp:{article:'3132',name:'NM Hyper Mid/End Cl\u0061mp',L:117.5,W:55.8,H:60,kg:.176}};
+export const FLOW_CP=JSON.parse(atob('eyJwYXJhbGxlbCI6eyJyb29mRWRnZV9wYW5lbEVkZ2UiOi0wLjM5NSwicm9vZk1pZF9wYW5lbEVkZ2UiOi0wLjMwNCwicm9vZkVkZ2VfcGFuZWxNaWQiOi0wLjI2Mywicm9vZk1pZF9wYW5lbE1pZCI6LTAuMjAyfSwiZWFzdHdlc3QiOnsicm9vZkVkZ2VfcGFuZWxFZGdlIjotMC4zMjksInJvb2ZNaWRfcGFuZWxFZGdlIjotMC4yNTMsInJvb2ZFZGdlX3BhbmVsTWlkIjotMC4xMzIsInJvb2ZNaWRfcGFuZWxNaWQiOi0wLjEwMn19'));
