6bcb22e05a6210b81bf18890828a09786ddd69fb
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-24 13:32:41 +0200

    Add Flow product records

--- src/lib/flow/flowConstants.js
+++ src/lib/flow/flowConstants.js
@@ -17,3 +17,8 @@ export const FLOW_PANEL_\u0057IDTH_RANGES = [
   { mode: 1, minMm: 984, maxMm: 1040 },
   { mode: 2, minMm: 1118, maxMm: 1174 },
 ];
+
+export const FLOW_PRODUCTS = {
+  to\u0077er: { article: '8000', name: 'NM Flow To\u0077er', L: 734, W: 283, H: 249, kg: 3.200 },
+  \u0077ing: { article: '8001', name: 'NM Flow \u0057ing', L: 950, W: 283, H: 67.5, kg: 3.000 },
+};
