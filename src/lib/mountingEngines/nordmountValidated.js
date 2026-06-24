ec2e3f0fae2bb405e506a58d0d42e5b56afd428b
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-24 14:18:11 +0200

    Import validated mounting engines

--- src/lib/mountingEngines/nordmountValidated.js
+++ src/lib/mountingEngines/nordmountValidated.js
@@ -1,4 +1,14 @@
-export { calculateNordmountRoof, calculateNordmountSnowLoad, calculateNordmountWindLoad, isNordmountProduct } from './nordmountValidatedParallel.js';
+import {
+  calculateNordmountRoof as calculateParallelRoof,
+  calculateNordmountSnowLoad,
+  calculateNordmountWindLoad,
+  isNordmountProduct,
+} from './nordmountValidatedParallel.js';
+import { FLOW_BRANCHES, FLOW_PRODUCTS } from '@/lib/flow/flowConstants.js';
+import { calculateFlowRoof } from '@/lib/flow/flowEngine.js';
+
+export { calculateNordmountSnowLoad, calculateNordmountWindLoad, isNordmountProduct };
+
 export function peakVelocityPressurePa(referenceWindMs, ridgeHeightM) {
   const meanWindMs = Number(referenceWindMs) || 0;
   return { z: Number(ridgeHeightM) || 2, cr: 1, meanWindMs, turbulenceIntensity: 0, peakPressurePa: 0.625 * meanWindMs * meanWindMs };
