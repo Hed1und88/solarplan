75607d0a5169702139d62d90b91e3cf34eab72e0
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-24 12:25:00 +0200

    Combine map viewport controller and fit lock

--- src/components/project/PanelMapViewportController.jsx
+++ src/components/project/PanelMapViewportController.jsx
@@ -1,1 +1,12 @@
-export { default } from './MapWorkbenchBehaviorController.jsx';
+import React from 'react';
+import MapWorkbenchBehaviorController from './MapWorkbenchBehaviorController.jsx';
+import MapViewportLock from './MapViewportLock.jsx';
+
+export default function PanelMapViewportController() {
+  return (
+    <>
+      <MapWorkbenchBehaviorController />
+      <MapViewportLock />
+    </>
+  );
+}
