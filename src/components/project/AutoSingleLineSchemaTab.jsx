5b72287372ac1668a358b3b4e1e854d9e6d235bc
Hed1und88 <119437650+Hed1und88@users.noreply.github.com>
2026-06-27 22:10:12 +0200

    Använd säker Enlinje-adapter

--- src/components/project/AutoSingleLineSchemaTab.jsx
+++ src/components/project/AutoSingleLineSchemaTab.jsx
@@ -1,11 +1,6 @@
-import React, { useMemo } from 'react';
-import AutoSingleLineSchemaView from './AutoSingleLineSchemaView.jsx';
-import { normalizeStringProductContext } from '@/lib/stringProductContext';
+import React from 'react';
+import AutoSingleLineSafeAdapter from './AutoSingleLineSafeAdapter.jsx';
 
 export default function AutoSingleLineSchemaEntry(props) {
-  const context = useMemo(
-    () => normalizeStringProductContext(props.project || {}, props.products || []),
-    [props.project?.string_layout_data, props.project?.solar_roof_planner_data, props.project?.panel_layout_data, props.products],
-  );
-  return React.createElement(AutoSingleLineSchemaView, { ...props, project: context.project, products: context.products });
+  return React.createElement(AutoSingleLineSafeAdapter, props);
 }
